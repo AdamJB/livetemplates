167d62664ef03e1fee42c8a92c7b8891c934ae18
Adam Bednarek <adam@ad60.com>
2014-08-03 10:30:41 -0400

    Fix up link..
    - Use hard link instead of soft
    - IntelliJ doesn't read soft links
    - Rename file to have no space.

--- makeLink.sh
+++ makeLink.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-ln -s ./Adam_s\ Android.xml ~/Library/Preferences/AndroidStudioBeta/templates/
+# Make a hard link, as AndroidStudio doesn't read soft linked files
+ln -h ./Adams_Android.xml ~/Library/Preferences/AndroidStudioBeta/templates/
