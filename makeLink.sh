91e3565549ae02c29e7b83d368b6391612d9fee1
Adam Bednarek <adambednarek@ITA-5158.local>
2015-06-10 10:04:05 -0400

    Update prefs folder to AS 1.2

--- makeLink.sh
+++ makeLink.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 # Make a hard link, as AndroidStudio doesn't read soft linked files
-ln -h ./Adams_Android.xml ~/Library/Preferences/AndroidStudioBeta/templates/
+# Change when updating AS ...
+ln -h ./Adams_Android.xml ~/Library/Preferences/AndroidStudio1.2/templates/
